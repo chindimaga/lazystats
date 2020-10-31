b4d849371049ca27a2e3f25e847356b66249f719
Restyled.io <commits@restyled.io>
2020-10-31 20:56:16 +0000

    Restyled by black

--- Uniform/_uniform.py
+++ Uniform/_uniform.py
@@ -6,10 +6,10 @@ class UniformGenerator:
         self.samples = list()
 
     def produce_samples_exclusive(self):
-        ''' Produce samples exclusive of upper and lower limit'''
-        gaps = (self.upper - self.lower)/(self.num_samples+1)
+        """ Produce samples exclusive of upper and lower limit"""
+        gaps = (self.upper - self.lower) / (self.num_samples + 1)
 
-        for i in range(1, self.num_samples+1):
-            self.samples.append(self.lower + i*gaps)
+        for i in range(1, self.num_samples + 1):
+            self.samples.append(self.lower + i * gaps)
 
         return self.samples
